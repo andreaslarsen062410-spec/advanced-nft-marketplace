26403d7b069ad870cc77be7b20389e5297048fe0
andreas <andreas.larsen062410@gmail.com>
2026-01-15 02:34:33 -0800

    setp 10

--- src/components/ConnectedAccountAddress.js
+++ src/components/ConnectedAccountAddress.js
@@ -4,4 +4,6 @@ import NavItem from './NavItem'
 
 export default function ConnectedAccountAddress ({ account }) {
   const accountUrl = `${chains.polygonMumbaiTestnet.explorers[0].url}/address/${account}`
+    return <NavItem title={shortenAddress(account)} href={accountUrl} openNewTab={true}/>
+  
 }
