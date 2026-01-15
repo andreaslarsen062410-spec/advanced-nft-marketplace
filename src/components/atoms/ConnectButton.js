f2302a473698d31df15f2b414f83030c5aa1b09f
andreas <andreas.larsen062410@gmail.com>
2026-01-15 02:18:19 -0800

    step 1

---
+++ src/components/atoms/ConnectButton.js
@@ -0,0 +1,7 @@
+import { Button } from '@mui/material'
+import { useContext, useEffect, useState } from 'react'
+import { Web3Context } from '../providers/Web3Provider'
+
+export default function ConnectButton () {
+
+}
