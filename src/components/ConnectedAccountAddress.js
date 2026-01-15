e51592f2cab1c8d8facf6102b9ec8399dd8cb334
andreas <andreas.larsen062410@gmail.com>
2026-01-15 02:34:17 -0800

    step 9

--- src/components/ConnectedAccountAddress.js
+++ src/components/ConnectedAccountAddress.js
@@ -1,3 +1,7 @@
 import { shortenAddress } from '../../utils/format'
 import { chains } from '../../utils/web3'
 import NavItem from './NavItem'
+
+export default function ConnectedAccountAddress ({ account }) {
+  const accountUrl = `${chains.polygonMumbaiTestnet.explorers[0].url}/address/${account}`
+}
