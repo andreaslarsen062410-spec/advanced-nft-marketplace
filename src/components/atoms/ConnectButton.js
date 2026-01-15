fb04f28d9ddede3108cf991855770cbc8e8ca865
andreas <andreas.larsen062410@gmail.com>
2026-01-15 02:19:09 -0800

    step 2

--- src/components/atoms/ConnectButton.js
+++ src/components/atoms/ConnectButton.js
@@ -3,5 +3,8 @@ import { useContext, useEffect, useState } from 'react'
 import { Web3Context } from '../providers/Web3Provider'
 
 export default function ConnectButton () {
+const { initializeWeb3 } = useContext(Web3Context)
+const [hasWindowEthereum, setHasWindowEthereum] = useState(false)
+
 
 }
