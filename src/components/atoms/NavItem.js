01833f785fd56d2443d87d06eabca7f3664d902e
andreas <andreas.larsen062410@gmail.com>
2026-01-18 08:43:39 -0800

    navbar item added button element

--- src/components/atoms/NavItem.js
+++ src/components/atoms/NavItem.js
@@ -8,7 +8,23 @@ export default function NavItem ({ title, href, openNewTab }) {
   const isActive = pathname === href
   return (
       <Link href={href} key={title} passHref>
-
+        <Button
+          component="a"
+          target={openNewTab && '_blank'}
+          style={{
+            margin: 'auto 0',
+            color: 'white',
+            display: 'block',
+            textDecoration: isActive && 'underline',
+            textAlign: 'center',
+            '&:hover': {
+              backgroundColor: '#fff',
+              color: '#3c52b2'
+            }
+          }}
+        >
+          {title}
+        </Button>
       </Link>
   )
 }
